bfd6befea37ea0a011809c18e419ddf7713ef688
Quazix0 <daniilabdyushev@yandex.ru>
2020-01-08 16:29:34 +0500

    Корабль

--- основа.py
+++ основа.py
@@ -7,10 +7,25 @@ class Settings():
     '''здесь будут храниться все настройки игры'''
 
     def __init__(self):
-        self.width = 1200
-        self.height = 800
+        self.width = 1000
+        self.height = 600
         self.fps = 60
         self.background_color = (230, 230, 230)
+
+
+class Ship():
+    '''наш корабль'''
+
+    def __init__(self, screen):
+        self.screen = screen
+        self.image = load_image('ship.png', -1)
+        self.rect = self.image.get_rect()
+        self.screen_rect = self.screen.get_rect()
+        self.rect.centerx = self.screen_rect.centerx
+        self.rect.bottom = self.screen_rect.bottom
+
+    def blit_ship(self):
+        self.screen.blit(self.image, self.rect)
 
 
 def load_image(name, colorkey=None):
@@ -32,6 +47,7 @@ def load_image(name, colorkey=None):
 pygame.init()
 settings = Settings()
 screen = pygame.display.set_mode((settings.width, settings.height))
+ship = Ship(screen)
 pygame.display.set_caption('')
 running = True
 while running:
@@ -39,4 +55,5 @@ while running:
         if event.type == pygame.QUIT:
             running = False
     screen.fill(settings.background_color)
+    ship.blit_ship()
 pygame.quit()
